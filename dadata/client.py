2274ccdd2f72d41b1faebdc522d183503fc4c31a
Nikolay Fominykh <nikolayfn@gmail.com>
2017-01-23 09:48:59 +0300

    simplification and some fixes

--- dadata/client.py
+++ dadata/client.py
@@ -1,24 +1,53 @@
 import requests
+from copy import deepcopy
+
+"""
+Ограничения:
+    не более 1 ФИО,
+    3 адресов,
+    3 телефонов,
+    3 email
+"""
+EMAIL_LIMIT = 3
+PHONE_LIMIT = 3
+ADDRESS_LIMIT = 3
+FIO_LIMIT = 1
+
+"""
+Helper Mixins
+"""
+class ManyOneMixin(object):
+    def _get_one(self):
+        return self.data[0] if self.data else None
+
+    def _set_one(self, value):
+        self.data.append(value)
+
+    one = property(_get_one, _set_one)
+
+    def _get_many(self):
+        return self.data
+
+    def _set_many(self, value):
+        self.data.extend(value)
+
+    many = property(_get_many, _set_many)
 
 
-class ApiURL(object):
+"""
+Обертка над Dadata API
+"""
+class ApiURL(ManyOneMixin):
+    limit = 1
+
     def __init__(self, url=None, **kwargs):
         self.url = url
+        self.data = []
 
-    def request(self, data):
-        pass
 
 
 class Address(ApiURL):
     pass
-
-
-HANDLERS = {
-    'default': lambda self, name: super(DaDataClient, self).__getattribute__(name),
-    'address': lambda self, name: Address(
-        url = self.url + 'address'
-    )
-}
 
 
 class DaDataClient(object):
@@ -33,6 +62,6 @@ class DaDataClient(object):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def __getattribute__(self, name):
-        handler = HANDLERS.get(name, HANDLERS.get('default'))
-        return handler(self, name)
+        self.address = Address(
+            url = self.url + 'address'
+        )
--- tests/test_url_builder.py
+++ tests/test_url_builder.py
@@ -3,6 +3,7 @@
 Проверяем создание урла для запроса
 """
 import unittest
+import random
 from dadata import DaDataClient
 
 
@@ -14,3 +15,16 @@ class UrlBuildTest(unittest.TestCase):
         url = self.client.address.url
         correct_url = self.client.defaults.get('url') + 'address'
         self.assertEqual(url, correct_url)
+
+    def test_address_one_empty(self):
+        self.assertEqual(self.client.address.one, None)
+
+    def test_address_one_set(self):
+        self.client.address.one = "Berkley Street 10"
+        self.assertEqual(self.client.address.one, "Berkley Street 10")
+
+    def test_address_many_set(self):
+        self.client.address.many = ["Berkley Street 10", "Another Nice Street"]
+        self.assertEqual(self.client.address.many, self.client.address.data)
+        self.assertEqual(self.client.address.many, ["Berkley Street 10", "Another Nice Street"])
+        self.assertEqual(self.client.address.one, "Berkley Street 10")
